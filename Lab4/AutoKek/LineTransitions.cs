2b2d6499c8fea7c8872bf7625c8187b04029cf6c
darkwindyclover <darkwindyclover@gmail.com>
2022-12-17 17:50:09 +0300

    lab4

---
+++ Lab4/AutoKek/LineTransitions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+   
+    partial class Form1
+    {
+        private void buttonRotateLine_Click(object sender, EventArgs e)
+        {
+           // buttonСhooseCentre.Visible = false;
+            textAngle.Text = "90";//по условию, повернуть ребро вокруг центра на 90 градусов
+            //координата центра отрезка
+            PointF p1 = polygonPoints[0];
+            PointF p2 = polygonPoints[1];
+            PointF pnew = new PointF((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);//ищем центр отрезка
+            rotateAroundPoint(pnew, canvas.Size);//поворачиваем
+            g.Clear(Color.White);
+            drawLineWithDot(polygonPoints[0], polygonPoints[1]);
+            textAngle.Clear();//для последующей работы
+
+
+        }
+
+        private void buttonCrossLines_Click(object sender, EventArgs e)
+        {
+            twolinesmode++;
+            
+        }
+        Tuple<float, float, float> GetFunc(PointF p1, PointF p2)//чтобы получить коэффициенты уравнения, через которое проходит прямая
+        {
+            float coef1 = p1.Y - p2.Y;
+            float coef2 =p2.X - p1.X;
+            float coef3 = p1.X * p2.Y - p2.X * p1.Y;
+            return Tuple.Create(coef1, coef2, coef3);
+        }
+        PointF CrossLines()
+        {
+            int count = polygonPoints.Count();
+            PointF p1 = polygonPoints[0];
+            PointF p2 = polygonPoints[1];
+            PointF p3 = polygonPoints[2];
+            PointF p4 = polygonPoints[3];
+            (float a1, float b1, float c1) = GetFunc(p1, p2);
+            (float a2, float b2, float c2) = GetFunc(p3, p4);
+            float divisor = a1 * b2 - a2 * b1;
+            if (divisor == 0)
+                return new PointF(int.MaxValue, int.MaxValue);
+            float x = (b1 * c2 - b2 * c1) / divisor;
+            float y = (c1 * a2 - c2 * a1) / divisor;
+            return new PointF(x, y);
+        }
+
+
+    }
+}
---
+++ Lab4/AutoKek/PointClassification.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab4
+{
+    partial class Form1
+    {
+        // Положение точки относительно направленного ребра
+        // yb·xa - xb·ya > 0 => b слева от Oa    
+        // yb·xa - xb·ya < 0 => b справа от Oa
+        private void ClassifyPoint(PointF edgeStart, PointF edgeEnd, PointF p)
+        {
+            float res = (p.X - edgeStart.X) * (edgeEnd.Y - edgeStart.Y) - (p.Y - edgeStart.Y) * (edgeEnd.X - edgeStart.X);
+            if (res > 0)
+                labelClassifyPoint.Text = "Точка находится слева от ребра";
+            else if (res < 0)
+                labelClassifyPoint.Text = "Точка находится справа от ребра";
+            else
+                labelClassifyPoint.Text = "Точка на отрезочке О_О Очень метко!";
+        }
+
+        // Положение точки относительно многоугольника
+        // Многоугольник будет выпуклым если при его обходе в каждой тройке последовательных вершин происходит поворот всегда в одну и ту же сторону.
+        // Если одно ребро многоугольника соответствует вектору AB, а следующее за ним ребро соответствует вектору BC, то направление поворота = AB.x* BC.y - AB.y* BC.x
+        private void PointInPolygon(PointF p)
+        {
+                 // определение выпуклости многоугольника
+                 CheckConvex(p, polygonPoints);
+
+                 // определение принадлежности точки многоугольнику
+                 CheckOuter(p, polygonPoints);
+        }
+
+        private void buttonIsPointInPolygon_Click(object sender, EventArgs e)
+        {
+            isPointInPolygonMode = true;
+            labelClassifyPoint.Text = "";
+
+        }
+
+        private void CheckConvex(PointF p, List<PointF> points)
+        {
+            bool isConvexPolygon = true;
+            bool sign = true, tempSign = true;
+            PointF ab = new PointF(points[1].X - points[0].X, points[1].Y - points[0].Y);
+            PointF bc = new PointF(points[2].X - points[1].X, points[2].Y - points[1].Y);
+            double rot = ab.X * bc.Y - ab.Y * bc.X; // направление поворота
+            if (rot < 0)
+                sign = false; // для проверки выпуклости
+
+            for (int i = 1; i < points.Count-1; i++)
+            {
+                ab = new PointF(points[i].X - points[i - 1].X, points[i].Y - points[i - 1].Y);
+                bc = new PointF(points[i+1].X - points[i].X, points[i+1].Y - points[i].Y);
+                rot = ab.X * bc.Y - ab.Y * bc.X;
+                if (rot < 0)
+                    tempSign = false;
+                else
+                    tempSign = true;
+                if (sign != tempSign) // если поворот отличается от остальных
+                    isConvexPolygon = false;
+            }
+
+            ab = new PointF(points[0].X - points[points.Count - 1].X, points[0].Y - points[points.Count - 1].Y);
+            bc = new PointF(points[1].X - points[0].X, points[1].Y - points[0].Y);
+            rot = ab.X * bc.Y - ab.Y * bc.X;
+            if (rot < 0)
+                tempSign = false;
+            if (sign != tempSign) // если поворот отличается от остальных
+                isConvexPolygon = false;
+            if (isConvexPolygon)
+                labelConvexPolygon.Text = "Выпуклый многоугольник";
+            else
+                labelConvexPolygon.Text = "Невыпуклый многоугольник";
+        }
+
+        private void CheckOuter(PointF p, List<PointF> points)
+        {
+            bool inPolygon = false;
+            int j = points.Count() - 1;
+            for (int i = 0; i < points.Count(); i++)
+            {
+                var p1 = points[i];
+                var p2 = points[j];
+                var checkY = p1.Y < p.Y && p2.Y >= p.Y || p2.Y < p.Y && p1.Y >= p.Y;
+                if (checkY)
+                {
+                    var checkX = p1.X + (p.Y - p1.Y) / (p2.Y - p1.Y) * (p2.X - p1.X) < p.X;
+                    if (checkX)
+                    {
+                        inPolygon = !inPolygon;
+                    }
+                }
+                j = i;
+            }
+            if (inPolygon)
+                labelPointInPolygon.Text = "Точка принадлежит";
+            else
+                labelPointInPolygon.Text = "Точка не принадлежит";
+        }
+
+        private void buttonClassifyPoint_Click(object sender, EventArgs e)
+        {
+            isPointClassifyMode = true;
+            buttonIsPointInPolygon.Enabled = false;
+            labelConvexPolygon.Text = "";
+            labelPointInPolygon.Text = "";
+        }
+    }
+}
